f11682ecfd2c3304a731d3825748667a724524d4
Kevin <kevindarmawan022@gmail.com>
2024-07-17 21:17:53 +0700

    Added footer to main layout.

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,7 @@
+ <footer class="flex-shrink-0 py-2 mt-4 container">
+     <div class="container text-center">
+         <a href="//github.com/kevintherm/skibidi-memes" class="link-dark">Github</a>
+         <a href="//instagram.com/kevindotdev" class="link-dark">Author</a>
+         <p>No Copyright &copy; Pake sesuka hati 🤣</p>
+     </div>
+ </footer>
